eea25782645049e7033024e92d74d4b4b5999aa4
Ben <bbcoumes@gmail.com>
2020-07-04 13:22:11 -0400

    Adding solution for maxchar exercise

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charCountMap = {};
+  let currMax = 0;
+  let currMaxChar = "";
+  for (let char of str) {
+    charCountMap[char] = charCountMap[char] ? charCountMap[char] + 1 : 1;
+    const count = charCountMap[char];
+    if (count > currMax) {
+      currMax = count;
+      currMaxChar = char;
+    }
+  }
+  return currMaxChar;
+}
 
 module.exports = maxChar;
