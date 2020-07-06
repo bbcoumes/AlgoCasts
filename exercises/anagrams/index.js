c4c88f8f60014f798bd981cfedc0cc7066e3ffac
Ben <bbcoumes@gmail.com>
2020-07-05 20:01:25 -0400

    Adding solution to annagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,40 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  return getSortedString(stringA) === getSortedString(stringB);
+}
+
+function getSortedString(str) {
+  return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+}
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//   const stringAarr = stringA.split(" ").join("").split("");
+//   const stringBarr = stringB.split(" ").join("").split("");
+//   const stringAMap = stringAarr.reduce(mapChars, {});
+//   const stringBMap = stringBarr.reduce(mapChars, {});
+//   const keysCountA = Object.keys(stringAMap).length;
+//   const keysCountB = Object.keys(stringBMap).length;
+//   if (keysCountA !== keysCountB) {
+//     return false;
+//   }
+//   for (let key in stringAMap) {
+//     const countA = stringAMap[key];
+//     const countB = stringBMap[key];
+//     if (countA !== countB) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function mapChars(charMap, char) {
+//   const alphaPattern = /[a-z]/g;
+//   if (char.match(alphaPattern)) {
+//     charMap[char.toLowerCase()] = charMap[char.toLowerCase()] + 1 || 1;
+//   }
+//   return charMap;
+// }
