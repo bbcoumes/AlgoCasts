a94cb5dee0da372547c97ae58128f54ebf3e8fba
Ben <bbcoumes@gmail.com>
2020-07-05 21:11:09 -0400

    Adding in solution for steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,22 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, numberOfSpaces = 0) {
+  const stepChar = "#";
+  const spaceChar = " ";
+  if (n === 0) {
+    return;
+  }
+  steps(n - 1, numberOfSpaces + 1);
+  console.log(stepChar.repeat(n) + spaceChar.repeat(numberOfSpaces));
+}
 
 module.exports = steps;
+
+// function steps(n) {
+//   const stepChar = "#";
+//   const spaceChar = " ";
+//   for (let i = 0; i < n; i++) {
+//     console.log(`${stepChar.repeat(i + 1)}${spaceChar.repeat(n - i - 1)}`);
+//   }
+// }
