5c1b6c6a82153274fcd02f9b628111387e52a9b5
Ben <bbcoumes@gmail.com>
2020-07-06 20:49:03 -0400

    Adding solution for pyramid question

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,22 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = "") {
+  const numberOfColumns = 2 * n - 1;
+  if (row === n) {
+    return;
+  }
+  if (level.length === numberOfColumns) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+  const midPoint = Math.floor(numberOfColumns / 2);
+  if (midPoint - row <= level.length && midPoint + row >= level.length) {
+    level += "#";
+  } else {
+    level += " ";
+  }
+  return pyramid(n, row, level);
+}
 
 module.exports = pyramid;
